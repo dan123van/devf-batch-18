45325648ee4ac395980b1244352dfec86fa4bd22
Daniel Silverio <danielsf123van@gmail.com>
2023-05-30 21:19:30 -0600

    Se agrego codigo para el dia 1 con IA

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const natural = require('natural');
+const classifier = new natural.BayesClassifier();
+
+classifier.addDocument('Este es un buen dia', 'positivo');
+classifier.addDocument('El clima es bueno hoy', 'positivo');
+classifier.addDocument('Este es un mal dia no aguanto el dolor de cabeza', 'negativo');
+classifier.addDocument('El trafico fue terrible hoy', 'negativo');
+classifier.addDocument('El sol es muy obscuro', 'negativo');
+classifier.addDocument('chale esta muy bueno', 'positivo');
+classifier.addDocument('chale que mal nimodo', 'negativo');
+classifier.train()
+
+console.log('1.- ', classifier.classify('hoy es un dia maravilloso'))
+
+console.log('2.- ', classifier.classify('El sol esta apagado'))
+
+console.log('3.- ', classifier.classify('Me fue mal hoy'))
+
+console.log('4.- ', classifier.classify('chale eso que te paso esta mal'))
+
+console.log('5.- ', classifier.classify('chale nimodo'))
+
+// Agregar console.logs O/Y addDocument con cualquier 
+// cosa que se les ocurra dejen su nombre en un comentario
